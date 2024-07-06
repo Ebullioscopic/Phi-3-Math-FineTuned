e3bd6243fc86ab44a45aad9074ea6cfb7cb277d0
Ebullioscopic <hrhn.mudaliar251@gmail.com>
2024-07-06 20:43:22 +0530

    updated main.py

--- README.md
+++ README.md
@@ -50,6 +50,9 @@ trainer = SFTTrainer(
 ```
 
 ## Run locally
+
+Via Ollama:
+
 ```bash
 ollama run Ebullioscopic/phi_3_math_finetuned
 ```
@@ -58,6 +61,55 @@ ollama run Ebullioscopic/phi_3_math_finetuned
 ```
 
 ```
+
 ## Fine Tuning
 
-Colab Notebook: []()
+JuPyter Notebook: [Open in Colab](https://github.com/Ebullioscopic/Phi-3-Math-FineTuned/blob/main/Phi_3_Fine_Tuning.ipynb)
+
+## Retrieval Augmented Generation
+
+Fine-tuning large language models like Phi-3 on massive datasets can be a slow crawl. Here's where Retrieval-Augmented Generation (RAG) comes in. RAG acts like a shortcut. It retrieves relevant information (think solved problems!) from a smaller, pre-built knowledge base when presented with a new math problem. Phi-3 then uses this retrieved context to understand and solve the problem itself.  This way, RAG leverages existing knowledge to speed up learning and potentially improve Phi-3's problem-solving accuracy without needing to train on the entire dataset. 
+
+### Run Locally
+Using Streamlit:
+```bash
+cd path/to/project
+python -m pip install -r requirements.txt
+streamlit run app/main.py
+```
+
+## Requirements
+
+- [Unsloth](https://huggingface.co/unsloth)
+- [Hugging Face Hub](https://huggingface.co/hub)
+- [Hugging Face Transformers](https://huggingface.co/docs/transformers/en/main)
+- [Hugging Face Datasets](https://huggingface.co/docs/datasets/en/main)
+- [Hugging Face Accelerate](https://huggingface.co/docs/accelerate/en/main)
+- [Hugging Face Trainer](https://huggingface.co/docs/transformers/en/main/trainers)
+- [Hugging Face PyTorch Lightning](https://huggingface.co/docs/transformers/en/main/training/pytorch-lightning)
+- [Streamlit](https://streamlit.io/)
+- [Ollama]()
+
+## References
+
+- [Jupyter notebooks with code examples for pre-training](https://github.com/aaaastark/Pretrain_Finetune_Transformers_Pytorch)
+- [Code and implementation details for BERT](https://github.com/google-research/bert)
+- [data loading, tokenization, and training](https://kedion.medium.com/fine-tuning-nlp-models-with-hugging-face-f92d55949b66)
+- [Pre-trained Transformers for Text Classification](https://towardsdatascience.com/an-introduction-to-fine-tuning-pre-trained-transformers-models-9ea546611664)
+- [BERT for Sentiment Analysis with TensorFlow 2.x and Hugging Face](https://pen_spark.medium.com/comprehensive-guide-to-bert-1e5e33b2b8c8)
+- [BERT with PyTorch](https://www.analyticsvidhya.com/blog/2022/11/comprehensive-guide-to-bert/)
+- [Text Classification Model](https://github.com/huggingface/notebooks/blob/main/examples/text_classification.ipynb)
+- [Question Answering Model with Hugging Face Transformers](https://huggingface.co/transformers/v3.3.1/custom_datasets.html)
+- [Transformers and fine-tuning concepts](https://www.coursera.org/specializations/deep-learning)
+- [Transformer architecture](https://arxiv.org/abs/1706.03762)
+- [BERT for various NLP tasks like question answering and textual entailment, comparing](https://arxiv.org/abs/2012.02462)
+- [Neural language models, including transformers](https://arxiv.org/abs/1510.00726)
+- [Hierarchical Structures in Transformers](https://arxiv.org/pdf/2210.09221)
+
+## Contributors
+
+[Hariharan Mudaliar](https://www.linkedin.com/in/hariharan-mudaliar)
+
+Tumati Omkar Chowdary
+
+[Akula Satya Sesha Sai](https://www.linkedin.com/in/akula-satya-sesha-sai-4994a924b/)
--- app/main.py
+++ app/main.py
@@ -1,155 +1,123 @@
-import streamlit as st
+# phi3_chat_interface.py
 from st_chat_message import message
-#from streamlit_chat import message
+import streamlit as st
 from streamlit.components.v1 import html
 from huggingface_hub import login
 import dotenv
 import os
-hutoken = os.getenv('HF_TOKEN')
-
-login(token=hutoken)
-def read_file(filename):
-  fh = open(filename, "r")
-  try:
-      return fh.read()
-  finally:
-      fh.close()
-import os
-import sys
-import psutil
-print("This is the current working directory: ",os.getcwd())
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, pipeline
-from time import time
-import transformers
-import torch
-chat_system_prompt = """
-    You are an advanced assistant that always greets users with catchy taglines for making them buy products named MobAI made for people visiting the Phone website.
-    You can help them find offers for devices, suggest devices based on their requirements, budget etc.
-    While displaying any offer related information, give the response in a tabular markdown format with columns Sr No, Offer, Provider, Offer amount and Final price of device after offer (calculate as per requirement).
-    If the user tries to ask out of topic questions do not engange in the conversation.
-    If the given context is not sufficient to answer the question,Do not answer the question.
-"""
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from langchain.llms import HuggingFacePipeline
-try:
-  from langchain_community.document_loaders import JSONLoader, TextLoader
-except:
-  from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import HuggingFaceDatasetLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_community.llms import HuggingFacePipeline
 
+# Load environment variables
+dotenv.load_dotenv()
+
+# Hugging Face login token
+#hutoken = os.getenv('HF_TOKEN')
+hutoken = "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"
+login(token=hutoken)
+
+# Define the chat system prompt
+chat_system_prompt = """
+    You are an advanced assistant that always greets users with catchy taglines for making them solve mathematical word problems.
+    You can help them solve problems based on their requirements, budget etc.
+    While solving any problem, give the response in a plain text format.
+    If the user tries to ask out of topic questions do not engage in the conversation.
+    If the given context is not sufficient to solve the problem, do not answer the question.
+"""
 
 @st.cache_resource
 def llm():
-    # Loading Model and Tokenizer
-    model_checkpoint = 'meta-llama/Meta-Llama-3-8B-Instruct'
-    model_config = AutoConfig.from_pretrained(model_checkpoint, trust_remote_code=True, max_new_tokens=1024, use_auth_token=True)
-
-    model = AutoModelForCausalLM.from_pretrained(model_checkpoint, trust_remote_code=True, config=model_config, device_map='auto', use_auth_token=True)
-
+    # Loading Model and Tokenizer for phi_3
+    model_checkpoint = 'Ebullioscopic/phi_3_math_full_fine_tuned'
+    model = AutoModelForCausalLM.from_pretrained(model_checkpoint, trust_remote_code=True, device_map='auto', use_auth_token=True)
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_auth_token=True)
 
     # Pipeline Creation
     pipy = pipeline("text-generation", model=model, tokenizer=tokenizer, torch_dtype=torch.float16, max_length=3000, device_map="auto")
-
     llmpipe = HuggingFacePipeline(pipeline=pipy)
     return llmpipe
-# Parsing function
-def parse(string):
-        return string.split("<|end_header_id|>")[-1]
 
-
-# Retriever
 @st.cache_resource
 def retriever():
-    try:
-        loader = JSONLoader("/content/kek/updated_Corrected_Data.json")
-    except:
-        loader = TextLoader("/content/kek/Alldata.txt")
-    
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_documents(documents)
+    # Loading the math dataset
+    dataset_loader = HuggingFaceDatasetLoader("Ebullioscopic/orca-math-word-problems-200k")
+    texts = dataset_loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    texts = text_splitter.split_documents(texts)
 
+    # Creating the vectorstore
     embedding_function = SentenceTransformerEmbeddings(model_name="BAAI/bge-small-en-v1.5")
-    vectorstore = Chroma(collection_name="sample_collection", embedding_function = embedding_function)
-
+    vectorstore = Chroma(collection_name="sample_collection", embedding_function=embedding_function)
     vectorstore.add_documents(texts)
 
+    # Creating the retriever
     retrieverdata = vectorstore.as_retriever(k=7)
     return retrieverdata
 
-# Pipeline Object
-# @st.cache_resource
 class Pipeline:
-    def __init__(self,llm,retriever):
+    def __init__(self, llm, retriever):
         self.llm = llm
         self.retriever = retriever
-    def retrieve(self,question):
-        docs = self.retriever.invoke(question)
+
+    def retrieve(self, problem):
+        docs = self.retriever.invoke(problem)
         return "\n\n".join([d.page_content for d in docs])
-    # You are a helpful, respectful and honest Sales assistant for Samsung that is made to give answers regarding offers for Samsung Products.
-    #            If the user asks for offers on any phone, give the offers in a tabular format with columns: Offer Provider, Offer Type, Offer Amount, Minimum Purchase Value, Offer Description, Final Price after offer (calculated accordingly).
-    #            The response should be given in Markdown format.
-    #            If the user tries to ask out of topic questions do not engange in the conversation.
-    #            If the given context is not sufficient to answer the question,Do not answer the question.
-    def augment(self,question,context):
+
+    def augment(self, problem):
         return f"""
-            <|begin_of_text|>
-            <|start_header_id|>
-            system
-            <|end_header_id|>
-            {chat_system_prompt}
-            <|eot_id|>
-            <|start_header_id|>
-            user
-            <|end_header_id|>
-            Answer the user question based on the context provided below
-            Context :{context}
-            Question: {question}
-            <|eot_id|>
-            <|start_header_id|>
-            assistant
-            <|end_header_id|>"""
-    def parse(self,string):
-        return string.split("<|end_header_id|>")[-1]
-    def generate(self,question):
-        context = self.retrieve(question)
-        prompt  = self.augment(question,context)
-        answer  = self.llm.invoke(prompt)
+        system
+        {chat_system_prompt}
+        user
+        Solve the problem based on the context provided below
+        Context: {self.retrieve(problem)}
+        Problem: {problem}
+        assistant
+        """
+
+    def parse(self, string):
+        return string
+
+    def generate(self, problem):
+        prompt = self.augment(problem)
+        answer = self.llm.invoke(prompt)
         return self.parse(answer)
-# @st.cache_resource    
+
+@st.cache_resource    
 def pipe():    
-    piped = Pipeline(llm(),retriever())
+    piped = Pipeline(llm(), retriever())
     return piped
+
 def chat(prompt):
-    if(prompt is not None):
+    if prompt is not None:
         output = pipe().generate(prompt)
         return output
+
 def on_input_change():
     user_input = st.session_state.user_input
-    print(repr(user_input))
     if len(user_input) == 0:
         return None
-    st.session_state.msgs.append({"content":str(user_input),"is_user":True})
+    st.session_state.msgs.append({"content": str(user_input), "is_user": True})
     value = chat(user_input)
-    st.session_state.msgs.append({"content":str(value),"is_user":False})
-    
+    st.session_state.msgs.append({"content": str(value), "is_user": False})
+
 if "msgs" not in st.session_state:
     st.session_state.msgs = [
         {
-            "content":"Hi, I am MobAI, your virtual assistant. Ask me any question about Phones. I will try to answer it. Thank you.",
-            "is_user":False,
+            "content": "Hi, I am MathAI, your virtual assistant. Ask me any mathematical word problem. I will try to solve it. Thank you.",
+            "is_user": False,
         },
     ]
+
 chat_placeholder = st.empty()
 
 with chat_placeholder.container():
     for idx, msg in enumerate(st.session_state.msgs):
         message(msg["content"], is_user=msg["is_user"], key=f"message_{idx}")
 
-
 with st.container():
-    st.text_input("Request:", on_change=on_input_change, key="user_input")
-    # create st.empty() container
+    st.text_input("Problem:", on_change=on_input_change, key="user_input")
+
